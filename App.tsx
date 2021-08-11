8fe242c9d84de59b8e9902ccbcb26b8d071d687e
Aleksandra Malecka <aleksandramalecka@pc-211.home>
2021-08-11 11:46:55 +0200

    drag&drop done

--- App.tsx
+++ App.tsx
@@ -1,21 +1,224 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback, useState } from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  LogBox,
+} from "react-native";
+import { Task } from "./src/Task";
+import RNPickerSelect from "react-native-picker-select";
+import DraggableFlatList, {
+  RenderItemParams,
+} from "react-native-draggable-flatlist";
+import "react-native-gesture-handler";
+
+// import { Task } from "./src/Task";
+LogBox.ignoreLogs([
+  "ReactNativeFiberHostComponent: Calling getNode() on the ref of an Animated component is no longer necessary. You can now directly use the ref instead. This method will be removed in a future release.",
+]);
+
+LogBox.ignoreLogs(["Remote debugger"]);
+
+type TaskPriority = "low" | "medium" | "high" | undefined;
+
+export interface ITask {
+  description: string;
+  priority: TaskPriority;
+  isDone: boolean;
+  createdAt: Date;
+  id: string;
+  onDrag: () => void;
+  isActive: boolean;
+}
+
+const initialTasks: ITask[] = [
+  {
+    description: "task description",
+    priority: "low",
+    isDone: false,
+    createdAt: new Date(),
+    id: new Date().toISOString(),
+    onDrag: () => {},
+    isActive: false,
+  },
+];
 
 export default function App() {
+  const [taskText, setText] = useState("");
+  const [taskItems, setTaskItems] = useState<ITask[]>(initialTasks);
+  const [searchText, setSearchText] = useState("");
+  const [isFiltered, setIsFiltered] = useState(false);
+  const [filteredArray, setFilteredArray] = useState<ITask[]>(initialTasks);
+  const [priority, setPriority] = useState();
+
+  console.log(taskItems);
+
+  const addTask = () => {
+    const newTask: ITask = {
+      description: taskText,
+      priority: priority,
+      isDone: false,
+      createdAt: new Date(),
+      id: new Date().toISOString(),
+    };
+    setTaskItems((tasksArray) => [newTask, ...tasksArray]);
+    setText("");
+  };
+
+  // console.log({ taskItems });
+  const handleDeleteTask = (id: ITask["id"]) => () => {
+    setTaskItems((tasksArray) => tasksArray.filter((task) => task.id !== id));
+  };
+
+  const handleTaskSave = (id: ITask["id"], newDescription: string) => {
+    setTaskItems((taskItems) =>
+      taskItems.map((task) =>
+        task.id === id ? { ...task, description: newDescription } : task
+      )
+    );
+  };
+
+  const filteredText = () => {
+    setFilteredArray(
+      taskItems.filter((text) =>
+        text.description.toLowerCase().includes(searchText.toLowerCase())
+      )
+    );
+    setIsFiltered(true);
+  };
+
+  const onOffFiltration = () => {
+    setSearchText("");
+    setIsFiltered(false);
+  };
+
+  const renderItem = useCallback(
+    ({ item, index, drag, isActive }: RenderItemParams<ITask>) => {
+      return (
+        <Task
+          description={item.description}
+          onDeletePress={handleDeleteTask(item.id)}
+          onTaskSave={handleTaskSave}
+          id={item.id}
+          priority={item.priority}
+          onDrag={drag}
+          isActive={isActive}
+        />
+      );
+    },
+    []
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        {/* <View style={styles.container}> */}
+        <Text>Todays tasks</Text>
+        {/* NEW TASK INPPUT + BUTTON */}
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={taskText}
+            onChangeText={setText}
+          />
+          <TouchableOpacity style={styles.button} onPress={addTask}>
+            <View>
+              <Text>+</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        {/* END - NEW TASK INPPUT + BUTTON */}
+
+        <View style={styles.search}>
+          <TextInput
+            style={styles.input}
+            value={searchText}
+            onChangeText={setSearchText}
+          />
+          {isFiltered ? (
+            <TouchableOpacity style={styles.button} onPress={onOffFiltration}>
+              <View>
+                <Text>x</Text>
+              </View>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity style={styles.button} onPress={filteredText}>
+              <View>
+                <Text style={{ fontSize: 10 }}>search</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        </View>
+
+        <View>
+          <RNPickerSelect
+            onValueChange={(priority) => setPriority(priority)}
+            items={[
+              { label: "low", value: "low" },
+              { label: "medium", value: "medium" },
+              { label: "high", value: "high" },
+            ]}
+          />
+        </View>
+
+        {/* <FlatList
+          data={isFiltered ? filteredArray : taskItems}
+          renderItem={({ item: task }) => (
+            <Task
+              description={task.description}
+              onDeletePress={handleDeleteTask(task.id)}
+              onTaskSave={handleTaskSave}
+              id={task.id}
+            />
+          )}
+        /> */}
+        <DraggableFlatList
+          data={taskItems}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => `draggable-item-${index}`}
+          onDragEnd={({ data }) => setTaskItems(data)}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 12,
+  },
+  input: {
+    borderWidth: 1,
+    width: 300,
+    height: 40,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    width: 40,
+  },
+  inputContainer: {
+    flexDirection: "row",
+  },
+  search: {
+    padding: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  picker: {
+    width: 200,
+    height: 220,
   },
 });
---
+++ src/Task.tsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Dimensions,
+} from "react-native";
+import { ITask } from "../App";
+import Checkbox from "react-native-checkbox-animated";
+
+const windowWidth = Dimensions.get("window").width;
+
+interface Props {
+  id: ITask["id"];
+  description: string;
+  priority: "low" | "medium" | "high";
+  onDeletePress: () => void;
+  onTaskSave: (id: ITask["id"], newDescription: string) => void;
+  isActive: boolean;
+  onDrag: () => void;
+}
+
+export const Task = ({
+  description,
+  onDeletePress,
+  onTaskSave,
+  id,
+  isActive,
+  onDrag,
+}: Props) => {
+  const [isEdited, setIsEdited] = useState<boolean>(false);
+  const [newDescription, setNewDescription] = useState<string>(description);
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <TouchableOpacity
+      onLongPress={onDrag}
+      style={{
+        ...styles.container,
+        backgroundColor: isActive ? "red" : "#11F323",
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Checkbox
+          label=""
+          onValueChange={() => setChecked(!checked)}
+          checked={checked}
+        />
+        {isEdited ? (
+          <TextInput
+            value={newDescription}
+            onChangeText={setNewDescription}
+            style={{ backgroundColor: "white" }}
+          />
+        ) : (
+          <Text style={styles.description}>{description}</Text>
+        )}
+
+        {/* <Text style={styles.description}>{`isEdited: ${isEdited}`}</Text> */}
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity
+          style={styles.deleteContainer}
+          onPress={onDeletePress}
+        >
+          <Text>-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.deleteContainer}
+          onPress={() => {
+            if (isEdited) {
+              onTaskSave(id, newDescription);
+            }
+            setIsEdited((prevValue) => !prevValue);
+          }}
+        >
+          <Text>{isEdited ? "SAVE" : "EDIT"}</Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: windowWidth / 1.3,
+    marginVertical: 16,
+    borderWidth: 1,
+    borderRadius: 12,
+    borderColor: "black",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#11F323",
+    padding: 4,
+  },
+  deleteContainer: {
+    justifyContent: "center",
+    borderWidth: 1,
+    borderRadius: 12,
+    alignItems: "center",
+    width: 40,
+    margin: 12,
+    height: 40,
+    backgroundColor: "#DDDDDD",
+  },
+  description: {
+    // flexGrow: 1,
+    // padding: 8,
+  },
+  // checkbox: {
+  //   paddingVertical: 50,
+  // }
+});
